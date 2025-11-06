75d25574134e84ce8f08e03a109808cfd820acfe
balldev1 <balldev1@hotmail.com>
2025-11-06 14:16:40 +0700

    dashboard.page -> jquery dashboard chart

---
+++ js/dashboard/chart.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const ctx = document.getElementById("myChart_1");
+
+  new Chart(ctx, {
+    type: "bar", // line, pie, doughnut, radar ฯลฯ
+    data: {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+      datasets: [
+        {
+          label: "Total Employee",
+          data: [100, 120, 150, 180, 200, 230],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
